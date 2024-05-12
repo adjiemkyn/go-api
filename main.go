8566f83db5dd1d66b755a648c893c3dfa0262702
adjiemkyn <adjiemikayana@gmail.com>
2024-05-12 10:38:41 +0800

    main

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"github.com/adjiemkyn/go-api/models"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	models.ConnectDatabase()
+
+	r.GET("/api/antrin", antriancontroller.Index)
+	r.GET("/api/antrian/:id", antriancontroller.Show)
+	r.POST("/api/antrian", antriancontroller.Create)
+	r.PUT("/api/antrian/:id", antriancontroller.Update)
+	r.DELETE("/api/antrian", antriancontroller.Delete)
+
+	r.Run()
 }
